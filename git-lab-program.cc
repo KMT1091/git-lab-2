c047fdc82afa6dd250a5bf19e88ffa085c4f2044
Kevin <kt131615@ohio.edu>
2020-01-22 19:28:34 -0500

    Hello Git

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+*
+* File: git-lab-program.cc
+* Author: Kevin Thompson
+* Date: January 22 2020
+* Description: Hello git program
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main (int argc, char const *argv[]){
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
